35d82eb63d2426adccdd456b19cc6a71520ab2c7
Joshua Mhar Alcubilla <103411030+mhardev@users.noreply.github.com>
2024-02-06 06:12:21 +0800

    Update index.js
    updating margin-top of title from education section

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -40,10 +40,10 @@ const Title = styled.div`
 font-size: 42px;
 text-align: center;
 font-weight: 600;
-margin-top: 20px;
+margin-top: 0px;
   color: ${({ theme }) => theme.text_primary};
   @media (max-width: 768px) {
-      margin-top: 12px;
+      margin-top: 0px;
       font-size: 32px;
   }
 `;
